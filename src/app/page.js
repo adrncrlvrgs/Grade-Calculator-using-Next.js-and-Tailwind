9c9fd14fcfbc93f45815ca4be82f14669538b714
Adrian <vargas.131251@caloocan.sti.edu.ph>
2023-07-10 02:22:22 +0800

    first-commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,118 @@
-import Image from 'next/image'
+'use client';
+import React,{useState} from 'react'
 
 export default function Home() {
+  const[formData, setFormData] = useState({prelims: '', midterm: '', prefinals: '', finals: ''});
+  const[result, setResult] = useState (0);
+
+  const handleChange = (e)=> {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    computation(formData);
+  };
+
+  const clear = () =>{
+    setFormData({prelims: '', midterm: '', prefinals: '', finals: ''});
+  }
+
+  const computation = (data) =>{
+
+    const percentage = {
+      prelims: 20/100, 
+      midterm: 20/100, 
+      prefinals: 20/100, 
+      finals: 40/100};
+
+    const cmptn = {
+      v1: data.prelims * percentage.prelims,
+      v2: data.midterm * percentage.midterm,
+      v3: data.prefinals * percentage.prefinals,
+      v4: data.finals * percentage.finals 
+    }
+    const result = cmptn.v1 + cmptn.v2 + cmptn.v3 + cmptn.v4;
+    setResult(result.toFixed(3));
+
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=" relative flex min-h-screen flex-col  p-10">
+      <div className=" flex z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex ">
+      
+        <div className="flex bottom-0 left-0 flex h-48 w-full items-end  bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none pb-5">
+         A simple grade calculator created via Next js with Tailwind <br/>
+         By Adrian Carl Vargas
         </div>
+        
+      </div>
+      <div className="grid grid-cols-8 gap-4 w-max self-center">
+        <div className="col-start-1 col-span-2 bg-gray-200 font-mono text-sm items-center justify-center rounded-lg">
+          <div className='items-center p-8'>
+            <p>Grading System:</p><hr className='border-t-2 border-gray-500'/><br/>
+            <p>Prelims {" "}: 20%</p><br/>
+            <p>Midterms {" "}: 20%</p><br/>
+            <p>Pre-finals {" "}: 20%</p><br/>
+            <p>Finals {" "}: 40%</p><br/>
+            <p>----------------</p><br/>
+            <p>Total {" "}: 100%</p>
+          </div>
+          
+        </div>
+        <div className="col-start-3 col-span-4">
+          <div className="place-item-center self-center pt-5">
+            <div className="flex justify-center w-full max-w-2xl ">
+              <form className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit} >
+              <p className='text-lg mb-3'>Grade Calculator</p>
+                <div className="flex flex-row justify-between space-x-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" >
+                    Prelims
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="prelims" type="number" placeholder="Enter your grade" value={formData.prelims} onChange={handleChange}/>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" >
+                    Midterms
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="midterm" type="number" placeholder="Enter your grade" value={formData.midterm} onChange={handleChange}/>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" >
+                    Pre-finals
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="prefinals" type="number" placeholder="Enter your grade" value={formData.prefinals} onChange={handleChange}/>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Finals
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="finals" type="number" placeholder="Enter your grade" value={formData.finals} onChange={handleChange}/>
+                </div>
+                </div>
+                <div className="flex items-center justify-center space-x-4">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Calculate
+                  </button>
+                  <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={clear}>
+                    Clear
+                  </button>
+                </div>
+                <div className="flex items-center justify-center p-5">
+                  <p className="text-xl">Result: {result}</p>
+                </div>
+              </form>
+            </div>
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none pb-5">
+              <p className='italic'>Disclaimer: This only computes the grade of specific subject</p>  
+            </div>
+          </div>
+        </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
